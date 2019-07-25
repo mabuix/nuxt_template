ce8b037a95f0362691760b2eabd9da69280e76f4
mabui <mabuix@gmail.com>
2019-07-25 15:59:05 +0900

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  extends: [
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: [
+    'prettier',
+    '@typescript-eslint'
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error'
+  }
+}
