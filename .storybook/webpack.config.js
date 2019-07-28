2080be86aff9d581aecf32cb2f5a8e6023f93d16
mabui <mabuix@gmail.com>
2019-07-28 12:14:37 +0900

    storybookのwebpack設定

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,5 +3,17 @@ const path = require('path')
 module.exports = async ({ config }) => {
   config.resolve.alias['~'] = path.resolve(__dirname, '../src')
   config.resolve.alias['assets'] = path.resolve(__dirname, '../src/assets')
+
+  config.module.rules.push({
+    test: /\.stylu?s?$/,
+    use: ['style-loader', 'css-loader', 'stylus-loader'],
+    include: path.resolve(__dirname, '../src/'),
+  })
+  config.module.rules.push({
+    test: /\.vue$/,
+    loader: 'storybook-addon-vue-info/loader',
+    enforce: 'post'
+  })
+
   return config
 }
